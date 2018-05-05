b1868a111861ecfbe0e332124814747cbb172ed1
dibyajyotiron <dibyajyoti.beingmyself.ghosal@gmail.com>
2018-05-05 14:14:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+  app = express(),
+  fs = require("file-system"),
+  port = process.env.PORT || 5000;
+app.use(express.static(__dirname + "/public"));
+app.get("/", function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", null, function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.write("File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+app.listen(port, function() {
+  console.log("App running");
+});
---
+++ public/style.js
@@ -0,0 +1,6 @@
+swal({
+    title: 'Simple Patatap Clone - A mini project by Dibyajyoti Ghosal. Press A to Z to have fun!',
+    width: 550,
+    padding: 40,
+    
+  })
